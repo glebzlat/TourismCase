e03a2a72332acf7273539ac8c77ba7f2dcaca450
glebzlat <mrkotofey@inbox.ru>
2024-11-09 22:51:45 +0500

    feat: add street map

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,21 @@
+import { Marker, Popup } from "react-leaflet";
+import { MapContainer } from "react-leaflet/MapContainer";
+import { TileLayer } from "react-leaflet/TileLayer";
+import styles from "./App.scss";
+
 function App() {
-  return <div><p>Hello, World!</p></div>;
+  let position = [51.09, 71.41];
+
+  return (
+    <div className={styles.mapContainer}>
+      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <Marker position={position}>
+          <Popup>Hello, World!</Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
 }
 
 export default App;
