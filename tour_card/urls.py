de5671cc3fe514c02413414082bde52fb5046abd
glebzlat <mrkotofey@inbox.ru>
2024-11-10 13:13:43 +0500

    feat: default url

--- tour_card/urls.py
+++ tour_card/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('menu.urls')),
     path('admin/', admin.site.urls),
     path('chat/', include('chat.urls')),
     path('accounts/', include('accounts.urls')),
